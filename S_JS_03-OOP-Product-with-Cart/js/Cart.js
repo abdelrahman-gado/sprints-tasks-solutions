4a099577d4c0dc1ff3f4bd97b554f2e3e1da5951
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2022-12-08 22:38:08 +0200

    refactoring the S_JS_03-OOP task

--- S_JS_03-OOP-Product-with-Cart/js/Cart.js
+++ S_JS_03-OOP-Product-with-Cart/js/Cart.js
@@ -1,5 +1,4 @@
-
-export default class Cart {
+class Cart {
   #_cartList;
 
   constructor(cartList) {
@@ -14,7 +13,7 @@ export default class Cart {
     this.#_cartList = newCartList;
   }
 
-  pushToCart(item) {
+  addToCart(item) {
     this.cartList.push(item);
   }
 
@@ -34,13 +33,92 @@ export default class Cart {
     return this.calculateSubTotal() + this.calculateShipping();
   }
 
-  removeCartLine(index) {
-    if (index < this.cartList.length) {
-      this.cartList.splice(index, 1);
-      return true;
-    } else {
-      return false;
-    }
+  incQuantityOfCartLine(i) {
+    this.cartList[i].incQuantity();
   }
 
+  decQuantityOfCartLine(i) {
+    this.cartList[i].decQuantity();
+  }
+
+  removeCartLine(index) {
+    this.cartList.splice(index, 1);
+  }
+
+  static getProductsFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("products") || "[]");
+  }
+
+  static getCartObject() {
+    /**
+     * Convert the json of products that lives in local storage to Cart object that holds all products.
+     */
+
+    const products = Cart.getProductsFromLocalStorage();
+    const cart = new Cart([]);
+
+    products.forEach((item) => {
+      const product = new Product(
+        item.productName,
+        item.productName,
+        item.price
+      );
+      const cartLine = new CartLine(item.id, product, item.quantity);
+      cart.addToCart(cartLine);
+    });
+
+    return cart;
+  }
+
+  getProductsArr() {
+    /**
+     * Convert the Cart object to array of product literal objects to store properties
+     *  of Cart object in json format.
+     */
+
+    const productsArr = [];
+    this.cartList.forEach((cartLineObj) => {
+      const tempObj = {
+        id: cartLineObj.id,
+        productName: cartLineObj.product.productName,
+        price: cartLineObj.product.price,
+        quantity: cartLineObj.quantity,
+      };
+
+      productsArr.push(tempObj);
+    });
+
+    return productsArr;
+  }
+
+  updateLocalStorage() {
+    /**
+     * Save changes after increasing, decreasing the quantity or even remove cart line in the local storage.
+     */
+    const productsArr = this.getProductsArr();
+    localStorage.setItem("products", JSON.stringify(productsArr));
+  }
+
+  render() {
+    const productsTbody = document.getElementById("products");
+    const subTotal = document.getElementById("sub-total");
+    const shipping = document.getElementById("shipping");
+    const total = document.getElementById("total");
+
+    // Firstly, we need remove rows in the tbody, sub-total, shipping and total and render the updated data again from products array.
+    productsTbody.innerHTML = "";
+    subTotal.textContent = "";
+    shipping.textContent = "";
+    total.textContent = "";
+
+    // Create products rows in tbody
+    this.cartList.forEach((cartLine, index) => {
+      productsTbody.innerHTML += cartLine.createCartLineHTMLRow(index);
+    });
+
+    // add sub-total, shipping and cartTotal values
+    subTotal.textContent = `$${this.calculateSubTotal()}`;
+    shipping.textContent = `$${this.calculateShipping()}`;
+    total.textContent = `$${this.calculateCartTotal()}`;
+  }
 }
--- S_JS_03-OOP-Product-with-Cart/js/Product.js
+++ S_JS_03-OOP-Product-with-Cart/js/Product.js
@@ -1,10 +1,12 @@
-export default class Product {
+class Product {
 
   #_productName;
+  #_image;
   #_price;
 
-  constructor(productName, price) {
+  constructor(productName, image, price) {
     this.#_productName = productName;
+    this.#_image = image;
     this.#_price = price;
   }
 
@@ -15,6 +17,14 @@ export default class Product {
   set productName(newProductName) {
     this.#_productName = newProductName;
   }
+
+  get image() {
+    return this.#_image;
+  }
+
+  set image(newImage) {
+    return this.#_image;
+  } 
 
   get price() {
     return this.#_price;
